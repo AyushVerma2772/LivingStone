7d5f37d3e6bf6a1c775199bbdacf5458f15b5425
Ayush verma <97657192+AyushVerma2772@users.noreply.github.com>
2022-09-11 12:19:47 +0530

    Update Home.js

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -4,7 +4,7 @@ import Testimonial from './Testimonial';
 
 import React from 'react'
 
-export default function Home() {
+const Home = () => {
     return (
         <>
             <HomeBg />
@@ -13,3 +13,5 @@ export default function Home() {
         </>
     )
 }
+
+export default Home
